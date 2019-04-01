bdfcf601618777d5facf59e4bc03e83dc57b84bf
siasree <csvidya455@gmail.com>
2019-04-01 16:11:26 +0530

    first commit

---
+++ js/task.js
@@ -0,0 +1,30 @@
+//alert("hello");
+function taskAdd(){
+    //alert("welcome");
+    var a=3;
+    var b=8;
+    var result=(a+b);
+    alert(result);
+};
+taskAdd();
+function taskSub(){
+    var c=4;
+    var d=7;
+    var result=(c-d);
+    alert(result);
+};
+taskSub();
+function taskMul(){
+    var e=5;
+    var f=6;
+    var result=(e*f);
+    alert(result);
+};
+taskMul();
+function taskDiv(){
+    var g=6;
+    var h=5;
+    var result=(g/h);
+    alert(result);
+};
+taskDiv();
